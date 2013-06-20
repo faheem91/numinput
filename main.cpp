a3d9e952399bb36a5c1652fa80003df0010772f6
faheem91 <faheem91@outlook.com>
2013-06-20 11:11:59 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+  int x; 
+  cout << "Please enter a number x: "; 
+  cin >> x; 
+  cout << endl; 
+  cout << "The square of the number is: " << x*x; 
+  system ("pause"); 
+  return 0; 
+  } 
